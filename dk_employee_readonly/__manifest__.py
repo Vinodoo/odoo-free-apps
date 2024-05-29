b43d0d9516612772d872792c7b89ae81ff315ed3
Vinoth-Nagaraj <vinoth@srikeshinfotech.com>
2024-05-29 14:12:51 +0530

    Make Employee Read-Only
    Make Employee Read-Only

---
+++ dk_employee_readonly/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Make Employee Read Only',
+    'version': '0.1',
+    'category': 'Generic Modules/Human Resources',
+    'summary': """
+        Make Employee Read Only
+    """,
+    'author': 'Do Kovai',
+    'company': 'Do Kovai',
+    'maintainer': 'Do Kovai',
+    'website': 'https://www.dokovai.com',
+    'depends': ['hr'],
+    'data': [
+        'views/hr_views.xml',
+    ],
+    'assets': {},
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'AGPL-3',
+}
